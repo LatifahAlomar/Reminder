9f883e78e5533a0f1415f26e57c4d074f7d21ec4
Najd Alsughaiyer <70834431+plateauh@users.noreply.github.com>
2021-02-21 01:39:38 +0300

    Cannot format given Object as a Date

--- Reminder/app/src/main/java/com/example/reminder/MainActivity.java
+++ Reminder/app/src/main/java/com/example/reminder/MainActivity.java
@@ -1,21 +1,34 @@
 package com.example.reminder;
 
+import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import android.text.InputType;
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     Dialog newRminder ; //this is the dialog for adding new reminder , in this dialog there is 3 input text the first for the title second for date thierd one for the time
     Button doneButton;
+    EditText reminder_title, reminder_date, reminder_time;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,17 +36,15 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         toolbar.setTitle("Reminder");
-
-               newRminder = new Dialog(this);
+        newRminder = new Dialog(this);
         newRminder.setContentView(R.layout.add_reminder_dialog);
+        reminder_title = newRminder.findViewById(R.id.title_input);
+        pickersListeners();
         doneButton = newRminder.findViewById(R.id.doneButton);
-
         doneButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 newRminder.dismiss();
-//
             }
         });
 //
@@ -42,12 +53,14 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 newRminder.show();
 //                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 //                        .setAction("Action", null).show();
             }
         });
+
+
+
     }
 @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -69,5 +82,57 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void pickersListeners(){
+        reminder_date = newRminder.findViewById(R.id.date_input);
+        reminder_time = newRminder.findViewById(R.id.time_input);
+
+        reminder_date.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                showDateDialog(reminder_date);
+            }
+
+            private void showDateDialog(final EditText reminder_date) {
+                final Calendar calendar = Calendar.getInstance();
+                DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        calendar.set(Calendar.YEAR, year);
+                        calendar.set(Calendar.MONTH, month);
+                        calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.UK);
+                        reminder_date.setText(simpleDateFormat.format(calendar));
+                    }
+                };
+                new DatePickerDialog(MainActivity.this, dateSetListener, calendar.get(Calendar.YEAR),
+                        calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+        // time picker
+        final EditText finalReminder_time = reminder_time;
+        reminder_time.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                showTimeDialog(finalReminder_time);
+            }
+
+            private void showTimeDialog(final EditText reminder_time) {
+                final Calendar calendar = Calendar.getInstance();
+                TimePickerDialog.OnTimeSetListener timeSetListener = new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                        calendar.set(Calendar.MINUTE, minute);
+                        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm", Locale.UK);
+                        reminder_time.setText(simpleDateFormat.format(calendar));
+                    }
+                };
+                new TimePickerDialog(MainActivity.this, timeSetListener, calendar.get(Calendar.HOUR_OF_DAY),
+                        calendar.get(Calendar.MINUTE), false).show();
+            }
+        });
+
     }
 }
