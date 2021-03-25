18e06554c0072bd49dc1a6c4e35d0448c5809af1
Najd Alsughaiyer <70834431+plateauh@users.noreply.github.com>
2021-03-25 17:16:58 +0300

    الحمدلله

--- Reminder/app/src/main/java/com/example/reminder/MainActivity.java
+++ Reminder/app/src/main/java/com/example/reminder/MainActivity.java
@@ -1,25 +1,32 @@
 package com.example.reminder;
 
+import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.util.Log;
+import android.text.format.DateFormat;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.DialogFragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,11 +36,13 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<remind_card> cardArrayList;
     Adapter listAdapter;
     RecyclerView recyclerView;
-    EditText dateInput;
+    static EditText dateInput, timeInput;
+    Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = getApplicationContext();
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         toolbar.setTitle("Reminder");
@@ -41,10 +50,18 @@ public class MainActivity extends AppCompatActivity {
         newRminder.setContentView(R.layout.add_reminder_dialog);
         doneButton = newRminder.findViewById(R.id.doneButton);
         dateInput = newRminder.findViewById(R.id.dateInput);
+        timeInput = newRminder.findViewById(R.id.TimeInput);
+
         dateInput.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.i("clicked", "date input was clicked");
+                showDatePickerDialog(v);
+            }
+        });
+        timeInput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTimePickerDialog(v);
             }
         });
         // RecyclerView
@@ -62,12 +79,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //database insertion
-                EditText priorityInput =newRminder.findViewById(R.id.Priority);
+//                ToggleButton priorityInput = (ToggleButton) findViewById(R.id.Priority);
+                Switch priorityInput = (Switch) findViewById(R.id.PrioritySwitch);
+                priorityInput.setChecked(true);
                 EditText titleInput = newRminder.findViewById(R.id.TitleEditText);
                 EditText dateInput = newRminder.findViewById(R.id.dateInput);
                 EditText timeInput = newRminder.findViewById(R.id.TimeInput);
+                boolean checked = priorityInput.isChecked();
+                String priority = ""+checked;
 
-                String priority = priorityInput.getText().toString();
+                Toast toast = Toast.makeText(context, priority, Toast.LENGTH_SHORT);
+                toast.show();
                 String title = titleInput.getText().toString();
                 String date = dateInput.getText().toString();
                 String time = timeInput.getText().toString();
@@ -82,13 +104,13 @@ public class MainActivity extends AppCompatActivity {
 
                     newRminder.dismiss();
                     Context context = getApplicationContext();
-                    Toast toast = Toast.makeText(context, "added a new reminder successfully", Toast.LENGTH_SHORT);
+                    toast = Toast.makeText(context, "added a new reminder successfully", Toast.LENGTH_SHORT);
                     toast.show();
                     updateList();
                 }
                 else{
                     Context context = getApplicationContext();
-                    Toast toast = Toast.makeText(context, "try again", Toast.LENGTH_SHORT);
+                    toast = Toast.makeText(context, "try again", Toast.LENGTH_SHORT);
                     toast.show();
                 }
             }
@@ -151,4 +173,67 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+
+
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Do something with the date chosen by the user
+            String date = dateTimeParser(month+1) + "/" + dateTimeParser(day) + "/" + year;
+            dateInput.setText(date);
+        }
+    }
+
+    public static class TimePickerFragment extends DialogFragment
+            implements TimePickerDialog.OnTimeSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current time as the default values for the picker
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+
+            // Create a new instance of TimePickerDialog and return it
+            return new TimePickerDialog(getActivity(), this, hour, minute,
+                    DateFormat.is24HourFormat(getActivity()));
+        }
+
+        @Override
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            // Do something with the time chosen by the user
+            String time = dateTimeParser(hourOfDay)+":"+dateTimeParser(minute);
+            timeInput.setText(time);
+        }
+    }
+    public static String dateTimeParser(int n){
+        String number = n + "";
+        switch (number.length()){
+            case 1: return "0"+number;
+            case 2: return number;
+        }
+        return "00";
+    }
 }
