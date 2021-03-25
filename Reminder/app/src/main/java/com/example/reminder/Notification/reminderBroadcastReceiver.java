1b07d348daba5ab1fab5b10c9204de01388b0264
Najd Alsughaiyer <70834431+plateauh@users.noreply.github.com>
2021-03-25 20:50:45 +0300

    yeeeesss

--- Reminder/app/src/main/java/com/example/reminder/Notification/reminderBroadcastReceiver.java
+++ Reminder/app/src/main/java/com/example/reminder/Notification/reminderBroadcastReceiver.java
@@ -1,5 +1,6 @@
 package com.example.reminder.Notification;
 
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -8,7 +9,9 @@ import android.widget.Toast;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
+import com.example.reminder.MainActivity;
 import com.example.reminder.R;
+import com.example.reminder.ReminderDetails;
 
 public class reminderBroadcastReceiver extends BroadcastReceiver {
     private static final String TAG = "MyBroadcastReceiver";
@@ -31,14 +34,15 @@ public class reminderBroadcastReceiver extends BroadcastReceiver {
             }
             break;
         }
-
+        Intent contentIntent= new Intent(context, ReminderDetails.class);
+        PendingIntent pendingContentIntent = PendingIntent.getActivity(context, 0, contentIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, channelId)
                 .setSmallIcon(R.drawable.redbutton)
                 .setContentTitle("Reminder")
                 .setContentText(title)
                 .setPriority(p)
                 // Set the intent that will fire when the user taps the notification
-//                .setContentIntent(intent)
+                .setContentIntent(pendingContentIntent)
                 .setAutoCancel(true);
 
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
